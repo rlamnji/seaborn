75f1ce28770e35fd58624d973bf3b6744a78bbe2
Minji Kim <rlamnji@naver.com>
2023-11-09 00:30:46 +0900

    Test1

--- examples/joint_kde.py
+++ examples/joint_kde.py
@@ -4,11 +4,15 @@ Joint kernel density estimate
 
 _thumb: .6, .4
 """
+# %%
 import seaborn as sns
 sns.set_theme(style="ticks")
 
 # Load the penguins dataset
 penguins = sns.load_dataset("penguins")
+
+# 추가된 함수 적용해보기
+# a = sns.print_a()
 
 # Show the joint distribution using kernel density estimation
 g = sns.jointplot(
